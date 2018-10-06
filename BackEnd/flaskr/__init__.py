d5f4173d3836cf45d6dc98f59019b861973af396
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-10-05 23:26:26 -0500

    addFav Charity

--- BackEnd/flaskr/__init__.py
+++ BackEnd/flaskr/__init__.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/charityMarket"
+    mongo = PyMongo(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
--- BackEnd/flaskr/charity.py
+++ BackEnd/flaskr/charity.py
@@ -3,8 +3,13 @@ import requests
 import json
 import base64
 from flask import (
-    Blueprint, redirect, request, jsonify
+    Flask, Blueprint, redirect, request, jsonify
 )
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/charityMarket"
+mongo = PyMongo(app)
 
 bp = Blueprint('charity', __name__, url_prefix='/charity')
 
@@ -13,17 +18,17 @@ def printHello():
     return('This is the Charity Endpoint')
 
 @bp.route('/addFav', methods=('GET', 'POST'))
-def addCharity():
-    ''' Request Type: POST - Input: User Auth Token - Output: Status of Success '''
+def addFav():
+    ''' Request Type: POST - Input: User Auth Token, Charity ID - Output: Status of Success '''
     if request.method == 'GET':
         return " ERROR 404 - GET REQUESTS NOT SERVICED AT THIS ENDPOINT "
-
-
-
-    return 'Nothing Yet'
+    authToken = request.form['authToken']
+    charityId = request.form['charityId']
+    mongo.db.Users.findOneandUpdate({'authId', authToken}, {'$addToSet': {'favCharities': [charityId]}})
+    return 'Inserted'
 
 @bp.route('/delFav', methods=('GET', 'POST'))
-def delCharity():
+def delFav():
     ''' Request Type: POST - Input: User Auth Token - Output: Status of Success '''
     if request.method == 'GET':
         return " ERROR 404 - GET REQUESTS NOT SERVICED AT THIS ENDPOINT "
