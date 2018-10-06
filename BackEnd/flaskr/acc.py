2bc5d3d64e28e6f52a8d4bef61cddd3be68f4b10
Butterscotch <butterscotch@Butterscotchs-MacBook-Pro.local>
2018-10-06 00:04:02 -0500

    updated acc

--- BackEnd/flaskr/acc.py
+++ BackEnd/flaskr/acc.py
@@ -3,10 +3,19 @@ import requests
 import json
 import base64
 from flask import (
-    Blueprint, redirect, request, jsonify
+    Blueprint, redirect, request, jsonify, Flask
 )
 
+from flask_pymongo import PyMongo
+app = Flask(__name__, instance_relative_config=True)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/charityMarket"
+mongo = PyMongo(app)
+
 bp = Blueprint('acc', __name__, url_prefix='/acc')
+
+mongo = None
+def getDb(db):
+    mongo = db
 
 @bp.route('/', methods=('GET', 'POST'))
 def printHello():
